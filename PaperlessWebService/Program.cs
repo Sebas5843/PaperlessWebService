1e9486f11f983cf096b649f677f1e46b6d89de82
slazo <slazo@NB-SLAZO>
2016-11-03 11:28:10 -0300

    Add project files.

---
+++ PaperlessWebService/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PaperlessWebService.PaperlessService;
+
+namespace PaperlessWebService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OnlinePortTypeClient cliente = new OnlinePortTypeClient("OnlineHttpSoap11Endpoint");
+
+            string text = System.IO.File.ReadAllText(@"C:\Users\slazo\TestFolder\WriteText.txt");
+
+            string respuesta = cliente.OnlineGeneration(94623000, "gen_sodexo", "abc123", text, 1, 0);
+            Console.WriteLine(1);
+        }
+    }
+}
